eaa5f58a3a4c35e8910f379312437eed8d43ac6b
Eric <eric@aishk.com>
2020-06-11 10:50:11 +0800

    Member Query API

---
+++ src/main/java/com/hybris/fortress/dao/CustomerDao.java
@@ -0,0 +1,4 @@
+package com.hybris.fortress.dao;
+
+public interface CustomerDao {
+}
---
+++ src/main/java/com/hybris/fortress/dao/impl/CustomerDaoImpl.java
@@ -0,0 +1,7 @@
+package com.hybris.fortress.dao.impl;
+
+import com.hybris.fortress.dao.CustomerDao;
+
+public class CustomerDaoImpl implements CustomerDao {
+
+}
